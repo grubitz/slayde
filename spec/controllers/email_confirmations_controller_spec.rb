46497426ee594f4e5a3b4fcf1db52f0b5db26f19
Grubitz <bcielinski@gmail.com>
2017-11-12 23:40:29 +0000

    fix of email confirmations controller

--- spec/controllers/email_confirmations_controller_spec.rb
+++ spec/controllers/email_confirmations_controller_spec.rb
@@ -2,11 +2,30 @@ require 'rails_helper'
 
 RSpec.describe EmailConfirmationsController, type: :controller do
 
-  describe "GET #confirm" do
-    it "returns http success" do
-      get :confirm
-      expect(response).to have_http_status(:success)
+  context 'for correct token' do
+    let(:user) { create(:user) }
+
+    it 'confirms email' do
+      expect_any_instance_of(User).to receive(:confirm!)
+      get :confirm, params: { token: user.confirmation_token }
+    end
+
+    it 'redirects to root' do
+      get :confirm, params: { token: user.confirmation_token }
+      expect(response).to redirect_to root_url
+    end
+
+    it 'shows notice' do
+      get :confirm, params: { token: user.confirmation_token }
+      expect(controller).to set_flash[:notice].to(/#{Regexp.escape(user.email)}/)
     end
   end
 
+
+  context 'for incorrect token' do
+    it 'raises exception' do
+      expect { get :confirm, params: { token: 'mindflyer' } }
+        .to raise_error(RuntimeError, 'Incorrect Token')
+    end
+  end
 end
