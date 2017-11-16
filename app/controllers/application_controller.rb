7d4e2b8154a5b82c17e98b3a69dc8829286d8723
Grubitz <bcielinski@gmail.com>
2017-11-16 22:16:14 +0000

    add admin privilages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,10 @@ class ApplicationController < ActionController::Base
     redirect_to new_session_url unless current_user.present?
   end
 
+  def authenticate_admin!
+    raise 'not authorised' unless current_user.present? && current_user.is_admin?
+  end
+
   def redirect_if_logged_in
     redirect_to root_url, notice: "You're already logged in!" if current_user.present?
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171021143337) do
+ActiveRecord::Schema.define(version: 20171116213620) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20171021143337) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "confirmation_token"
+    t.boolean "is_admin", default: false, null: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
   end
