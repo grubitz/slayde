1b3b884e5db41354c845a3afe3c3f967d0654589
Grubitz <bcielinski@gmail.com>
2017-12-19 10:38:09 +0000

    code cleaned

--- spec/controllers/password_resets_controller_spec.rb
+++ spec/controllers/password_resets_controller_spec.rb
@@ -11,19 +11,19 @@ RSpec.describe PasswordResetsController, type: :controller do
     context 'for valid email' do
       let(:user) { create(:user_confirmed) }
       it 'sends reset password email' do
-        expect { post :create, params: { email: user.email } }.to change { ActionMailer::Base.deliveries.count }.by(1)
+        expect { post :create, params: { email: user.email } }
+            .to change { ActionMailer::Base.deliveries.count }.by(1)
       end
-
       it 'redirects to root_url with proper alert' do
         post :create, params: { email: user.email }
         expect(response).to redirect_to root_url
         expect(controller).to set_flash[:notice].to(/#{user.email}/)
       end
-
     end
     context 'for invalid email' do
       it 'does not send reset password email' do
-        expect { post :create, params: { email: 'jake.peralta@nypd.com' } }.not_to change { ActionMailer::Base.deliveries.count }
+        expect { post :create, params: { email: 'jake.peralta@nypd.com' } }
+            .not_to change { ActionMailer::Base.deliveries.count }
       end
       it 'redirects to root_url with proper alert' do
         post :create, params: { email: 'jake.peralta@nypd.com' }
