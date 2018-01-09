a00b68ae9178e35a7a3cbcf96c50f0c09724d4ab
Grubitz <bcielinski@gmail.com>
2018-01-09 13:09:42 +0000

    test coverage

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -3,7 +3,9 @@ require 'rails_helper'
 RSpec.describe ApplicationHelper, type: :helper do
 
   it 'finds currently logged in user' do
-
+    user = create(:user_confirmed)
+    sign_in(user)
+    expect(current_user).to eq(user)
   end
 
 end
